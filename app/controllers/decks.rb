8dd0f60c081e404680ebca21a1bfbca15b4829ec
Matt Grimm <m.m.grimm1@gmail.com>
2016-10-28 14:22:45 -0500

    Start deck controller

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,5 +1,17 @@
 get '/decks' do
-
   @decks = Deck.all
   erb :"decks/index"
+end
+
+get '/decks/:deck_id/cards/:card_id' do
+
+  @deck = Deck.find_by(params[:deck_id])
+  @card = Card.find_by(params[:card_id])
+
+
+end
+
+post '/decks/:deck_id/cards/:card_id' do
+
+  redirect "/decks/:deck_id/cards/#{card.id}"
 end
