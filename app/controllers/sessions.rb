03fa4cf9ec20144093de022258de3c6900ed3f89
Corbin Via & Joan Petersohn <pair+corbin1987.jepetersohn@devbootcamp.com>
2016-10-28 11:33:01 -0500

    changed parameters of authenticate in post sessions route

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -4,7 +4,7 @@ end
 
 post '/sessions' do
   @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:username], params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/decks'
     else
