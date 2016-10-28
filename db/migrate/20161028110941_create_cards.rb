dd0ea77da98148669419d817fcde6707d3cb6836
Matt Grimm <m.m.grimm1@gmail.com>
2016-10-28 14:42:52 -0500

    Add column already_guessed to card migration.

--- db/migrate/20161028110941_create_cards.rb
+++ db/migrate/20161028110941_create_cards.rb
@@ -5,6 +5,7 @@ class CreateCards < ActiveRecord::Migration
       t.string :answer, { null: false }
       t.integer :deck_id
       t.boolean :correct, default: false
+      t.boolean :already_guessed, default: false
 
       t.timestamps(null: false)
     end
