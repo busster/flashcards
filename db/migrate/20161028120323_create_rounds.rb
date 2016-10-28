72b766676b09ab4684ab1e02399be097cd8642e3
Corbin Via & Joan Petersohn <pair+corbin1987.jepetersohn@devbootcamp.com>
2016-10-28 14:44:37 -0500

    created round helper

--- db/migrate/20161028120323_create_rounds.rb
+++ db/migrate/20161028120323_create_rounds.rb
@@ -1,8 +1,8 @@
 class CreateRounds < ActiveRecord::Migration
   def change
     create_table :rounds do |t|
-      t.integer :total_guesses
-      t.integer :first_try
+      t.integer :total_guesses, default: 0
+      t.integer :first_try, default: 0
       t.integer :user_id
 
       t.timestamps(null: false)
