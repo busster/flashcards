6051c396fd1f89b7f981d49c64e95dfae18916a9
Corbin Via & Joan Petersohn <pair+corbin1987.jepetersohn@devbootcamp.com>
2016-10-28 18:03:18 -0500

    Still working on ending rounds

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -3,7 +3,7 @@ get '/decks/:deck_id/cards/:card_id' do
   @deck = Deck.find(params[:deck_id])
   @card = Card.find(params[:card_id])
 
-  if Round.find_by(user_id: current_user.id, deck_id: params[:deck_id] )
+  if Round.find_by(user_id: current_user.id, deck_id: params[:deck_id]) && @deck.cards.where
     @round = Round.find_by(user_id: current_user.id, deck_id: params[:deck_id] )
     erb :'cards/show'
   else
@@ -14,33 +14,27 @@ get '/decks/:deck_id/cards/:card_id' do
 
 end
 
-
-
 post '/decks/:deck_id/cards/:card_id' do
-
-
   user = User.find(session[:user_id])
-
+  round = Round.where(user_id: current_user.id, deck_id: params[:deck_id]).last
   card = Card.find(params[:card_id])
-  user.rounds.last.total_guesses += 1
-
+  @current_round = user.rounds.last
+  @current_round.update_attributes(total_guesses: (@current_round.total_guesses += 1))
   if !card.already_guessed && card.correct?(params[:answer])
-    user.rounds.last.first_try += 1
+   @current_round.update_attributes(first_try: (@current_round.first_try += 1))
     card.correct_answer
   elsif card.correct?(params[:answer])
     card.correct_answer
   else
     card.already_guessed = true
   end
-
-
   cards = Deck.find_by(id: params[:deck_id]).cards
   card = cards.where(correct: false).sample
   if card
     redirect "/decks/#{params[:deck_id]}/cards/#{card.id}"
   else
     Deck.find_by(id: params[:deck_id]).cards.update_all(correct: false, already_guessed: false)
-    redirect "/"
+    redirect "/decks/#{params[:deck_id]}/rounds/#{@current_round.id}"
   end
 
 end
