94f8bfc3941885b1a21df5692f9b3052db41574f
[01;31m[K"name"[m[K: "Stephanie Lane <[01;31m[K"email"[m[K: null>
2016-10-30 21:12:17 -0500

    added 404 error page

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -3,14 +3,18 @@ get '/decks/:deck_id/cards/:card_id' do
   @deck = Deck.find(params[:deck_id])
   @card = Card.find(params[:card_id])
 
-  if Round.find_by(user_id: current_user.id, deck_id: params[:deck_id]) && @deck.cards.where
-    @round = Round.find_by(user_id: current_user.id, deck_id: params[:deck_id] )
-    erb :'cards/show'
-  else
+  if logged_in?
+    if Round.find_by(user_id: current_user.id, deck_id: params[:deck_id]) && @deck.cards.where
+      @round = Round.find_by(user_id: current_user.id, deck_id: params[:deck_id] )
+      erb :'cards/show'
+    else
       @round = Round.create(user_id: current_user.id, deck_id: params[:deck_id])
       @current_round = current_user.rounds.last
       erb :'cards/show'
-  end
+    end
+  else 
+    erb :"404"
+  end 
 
 end
 
